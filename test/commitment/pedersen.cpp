22190f60ea4ba9ff7e57edddc017e5661aaaddd0
ChernyshovaP <42933210+tshchelovek@users.noreply.github.com>
2022-03-18 13:11:52 +0300

    Minor changes

--- test/commitment/pedersen.cpp
+++ test/commitment/pedersen.cpp
@@ -53,7 +53,7 @@ BOOST_AUTO_TEST_CASE(pedersen_basic_test) {
     using curve_type = algebra::curves::bls12<381>;
     using curve_group_type = curve_type::template g1_type<>;
     using field_type = typename curve_type::scalar_field_type;
-    using multiexp_type = typename algebra::policies::multiexp_method_BDLO12;
+    typedef typename algebra::policies::multiexp_method_BDLO12 multiexp_type;
 
     constexpr static const std::size_t n = 30;
     constexpr static const std::size_t k = 10;

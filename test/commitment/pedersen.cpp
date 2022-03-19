f4f02121788f49e882cf340ddb14caaabc9e8220
ChernyshovaP <42933210+tshchelovek@users.noreply.github.com>
2022-03-19 19:07:54 +0300

    Added test (total 2)

--- test/commitment/pedersen.cpp
+++ test/commitment/pedersen.cpp
@@ -65,7 +65,6 @@ BOOST_AUTO_TEST_CASE(pedersen_basic_test) {
     while (g == h) {
         h = algebra::random_element<curve_group_type>();
     }
-    // std::cout << "g and h: " << g.X.data << ' ' << g.Y.data << ' ' << g.Z.data << ", " << h.X.data << ' ' << h.Y.data << ' ' << h.Z.data << '\n';
 
     typedef typename zk::commitments::pedersen<curve_type, multiexp_type> pedersen_type;
 
@@ -85,18 +84,66 @@ BOOST_AUTO_TEST_CASE(pedersen_basic_test) {
 
     // commit
     constexpr static const field_type::value_type w = field_type::value_type(543);
-    // std::cout << "secret message: " << w.data << ' ' << (g + g + g + g + g == w * g) << '\n';
 
     // eval
     proof_type proof = pedersen_type::proof_eval(params, w);
-    // std::cout << "commitment value: " << proof.E_0.X.data << ' ' << proof.E_0.Y.data << ' ' << proof.E_0.Z.data << '\n';
 
     // verify
-    // BOOST_CHECK(proof.E_0 == params.g * proof.pk[0].s + params.h * proof.pk[0].t);
-    // BOOST_CHECK(proof.pk[0].s == w);
     BOOST_CHECK(pedersen_type::verify_eval(params, proof));
     
-    std::vector<std::size_t> idx = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    std::vector<std::size_t> idx;
+    for (size_t i = 1; i <= k; ++i) {
+        idx.push_back(i);
+    }
+    BOOST_CHECK(idx.size() >= k);
+    BOOST_CHECK(w == pedersen_type::message_eval(params, proof, idx));
+}
+
+BOOST_AUTO_TEST_CASE(pedersen_long_test) {
+
+    // setup
+    using curve_type = algebra::curves::bls12<381>;
+    using curve_group_type = curve_type::template g1_type<>;
+    using field_type = typename curve_type::scalar_field_type;
+    typedef typename algebra::policies::multiexp_method_BDLO12 multiexp_type;
+
+    constexpr static const std::size_t n = 20000000;
+    constexpr static const std::size_t k = 500000;
+    static curve_group_type::value_type g = algebra::random_element<curve_group_type>();
+    static curve_group_type::value_type h = algebra::random_element<curve_group_type>();
+    while (g == h) {
+        h = algebra::random_element<curve_group_type>();
+    }
+
+    typedef typename zk::commitments::pedersen<curve_type, multiexp_type> pedersen_type;
+
+    typedef typename pedersen_type::proof_type proof_type;
+    typedef typename pedersen_type::params_type params_type;
+
+    params_type params;
+
+    params.n = n;
+    params.k = k;
+    params.g = g;
+    params.h = h;
+
+    BOOST_CHECK(g != h);
+    BOOST_CHECK(n >= k);
+    BOOST_CHECK(k > 0);
+
+    // commit
+    constexpr static const field_type::value_type w = field_type::value_type(1234567890);
+
+    // eval
+    proof_type proof = pedersen_type::proof_eval(params, w);
+
+    // verify
+    BOOST_CHECK(pedersen_type::verify_eval(params, proof));
+    
+    std::vector<std::size_t> idx;
+    for (size_t i = 1; i <= k + 5; ++i) {
+        idx.push_back(i);
+    }
     BOOST_CHECK(idx.size() >= k);
     BOOST_CHECK(w == pedersen_type::message_eval(params, proof, idx));
 }

4d3c753aebfdb6f73efd4983e0cdc3cd05f1a370
ChernyshovaP <42933210+tshchelovek@users.noreply.github.com>
2022-03-20 19:18:15 +0300

    Update pedersen.hpp

--- include/nil/crypto3/zk/commitments/polynomial/pedersen.hpp
+++ include/nil/crypto3/zk/commitments/polynomial/pedersen.hpp
@@ -43,7 +43,7 @@ namespace nil {
                 template<typename CurveType, typename MultiexpMethod>
                 class pedersen {
                 public:
-                    typedef typename CurveType::scalar_field_type field_type;
+                    typedef typename CurveType::base_field_type field_type;
                     typedef typename CurveType::template g1_type<> group_type;
                     typedef typename field_type::value_type evaluation_type;
                     typedef typename group_type::value_type commitment_type;

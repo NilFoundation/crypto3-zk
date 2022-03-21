5826f7b7bb3eef26277e5b1ebe45151a049b8625
ChernyshovaP <42933210+tshchelovek@users.noreply.github.com>
2022-03-21 17:59:47 +0300

    Update pedersen.cpp

--- test/commitment/pedersen.cpp
+++ test/commitment/pedersen.cpp
@@ -102,8 +102,7 @@ BOOST_AUTO_TEST_CASE(pedersen_basic_test) {
     
     BOOST_CHECK(idx.size() >= k);
     field_type::value_type secret = pedersen_type::message_eval(params, proof, idx);
-    std::cout << "secret: " << secret.data << '\n';
-    BOOST_CHECK((secret == 0) || (w == secret));
+    BOOST_CHECK(w == secret);
 }
 
 BOOST_AUTO_TEST_CASE(pedersen_short_test) {
@@ -147,12 +146,77 @@ BOOST_AUTO_TEST_CASE(pedersen_short_test) {
     BOOST_CHECK(pedersen_type::verify_eval(params, proof));
 
     std::vector<int> idx;
-    for (int i = 1; i <= k; ++i) {
-        idx.push_back(i);
+    std::vector<int> idx_base;
+    for (int i = 1; i <= n; ++i) {
+        idx_base.push_back(i);
     }
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(idx_base.begin(), idx_base.end(), gen);
+    for (int i = 0; i < k; ++i) {
+        idx.push_back(idx_base[i]);
+    }
+    
     BOOST_CHECK(idx.size() >= k);
     field_type::value_type secret = pedersen_type::message_eval(params, proof, idx);
-    BOOST_CHECK((secret == 0) || (w == secret));
+    BOOST_CHECK(w == secret);
+}
+
+BOOST_AUTO_TEST_CASE(pedersen_long_test) {
+
+    // setup
+    using curve_type = algebra::curves::bls12<381>;
+    using curve_group_type = curve_type::template g1_type<>;
+    using field_type = typename curve_type::scalar_field_type;
+
+    constexpr static const int n = 2000000000;
+    constexpr static const int k = 1999999999;
+    static curve_group_type::value_type g = algebra::random_element<curve_group_type>();
+    static curve_group_type::value_type h = algebra::random_element<curve_group_type>();
+    while (g == h) {
+        h = algebra::random_element<curve_group_type>();
+    }
+
+    typedef typename zk::commitments::pedersen<curve_type> pedersen_type;
+
+    typedef typename pedersen_type::proof_type proof_type;
+    typedef typename pedersen_type::params_type params_type;
+
+    params_type params;
+
+    params.n = n;
+    params.k = k;
+    params.g = g;
+    params.h = h;
+
+    BOOST_CHECK(g != h);
+    BOOST_CHECK(n >= k);
+    BOOST_CHECK(k > 0);
+
+    // commit
+    constexpr static const field_type::value_type w = field_type::value_type(300000000);
+
+    // eval
+    proof_type proof = pedersen_type::proof_eval(params, w);
+
+    // verify
+    BOOST_CHECK(pedersen_type::verify_eval(params, proof));
+
+    std::vector<int> idx;
+    std::vector<int> idx_base;
+    for (int i = 1; i <= n; ++i) {
+        idx_base.push_back(i);
+    }
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::shuffle(idx_base.begin(), idx_base.end(), gen);
+    for (int i = 0; i < k; ++i) {
+        idx.push_back(idx_base[i]);
+    }
+    
+    BOOST_CHECK(idx.size() >= k);
+    field_type::value_type secret = pedersen_type::message_eval(params, proof, idx);
+    BOOST_CHECK(w == secret);
 }
 
 BOOST_AUTO_TEST_SUITE_END()

f9769e1faf333bb389c9091ce484abf3a8ac3549
ChernyshovaP <42933210+tshchelovek@users.noreply.github.com>
2022-03-21 17:51:05 +0300

    Update pedersen.cpp

--- test/commitment/pedersen.cpp
+++ test/commitment/pedersen.cpp
@@ -94,9 +94,9 @@ BOOST_AUTO_TEST_CASE(pedersen_basic_test) {
         idx_base.push_back(i);
     }
     std::random_device rd;
-    std::mt19937 g(rd());
-    std::shuffle(idx_base.begin(), idx_base.end(), g);
-    for (int i = 0; i < pubk.k; ++i) {
+    std::mt19937 gen(rd());
+    std::shuffle(idx_base.begin(), idx_base.end(), gen);
+    for (int i = 0; i < k; ++i) {
         idx.push_back(idx_base[i]);
         std::cout << idx[i] << ' ';
     }

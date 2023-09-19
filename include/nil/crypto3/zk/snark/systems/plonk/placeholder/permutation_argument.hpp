936ade04fa50dc64d1b416a6fd13401c165b148d
Vasiliy Olekhov <vasiliy.olekhov@nil.foundation>
2023-09-19 17:38:12 +0300

    Add two more std::move-s, less copies

--- include/nil/crypto3/zk/snark/systems/plonk/placeholder/permutation_argument.hpp
+++ include/nil/crypto3/zk/snark/systems/plonk/placeholder/permutation_argument.hpp
@@ -174,7 +174,7 @@ namespace nil {
                         F_dfs[2] *= V_P;
                         F_dfs[2] *= preprocessed_data.q_last;
 
-                        prover_result_type res = {F_dfs, V_P, V_P_tree};
+                        prover_result_type res = {std::move(F_dfs), std::move(V_P), V_P_tree};
 
                         return res;
                     }

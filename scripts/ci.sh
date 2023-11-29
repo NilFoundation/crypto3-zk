8aba1d828a4f47cb77d39537568ef5fc3b25e86d
Vasiliy Olekhov <vasiliy.olekhov@nil.foundation>
2023-11-29 13:03:43 +0300

    CI scripts update #238

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,13 +2,4 @@
 
 set -vxe
 
-rm -rf suite
-mkdir -p suite && cd suite
-
-SUITE_REPO=https://github.com/NilFoundation/crypto3
-
-git clone --depth=1 --recursive ${SUITE_REPO}
-
-pushd suite/crypto3/libs/zk
-git switch ${GIT_BRANCH}
-popd
+echo "Scripted tests"

f18a9116496311179a322d65a8049be9ea1ce672
Vasiliy Olekhov <vasiliy.olekhov@nil.foundation>
2023-11-29 14:16:03 +0300

    Add tests to script #238

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,9 +13,23 @@ cmake -DCMAKE_BUILD_TYPE=Debug \
     ..
 
 test_targets="crypto3_zk_commitment_fold_polynomial_test \
-    crypto3_zk_commitment_fri_test crypto3_zk_commitment_lpc_test"
+    crypto3_zk_commitment_fri_test \
+    crypto3_zk_commitment_lpc_test \
+    crypto3_zk_commitment_kzg_test \
+    crypto3_zk_systems_plonk_placeholder_placeholder_test \
+    crypto3_zk_commitment_powers_of_tau_test \
+    crypto3_zk_commitment_proof_of_knowledge_test \
+    crypto3_zk_commitment_r1cs_gg_ppzksnark_mpc_test \
+    crypto3_zk_math_expression_test \
+    crypto3_zk_systems_plonk_plonk_constraint_test \
+    crypto3_zk_commitment_proof_of_knowledge_test \
+    crypto3_zk_transcript_transcript_test"
 
 for t in $test_targets ; do
     cmake --build . -t $t
 done
 
+for t in $test_targets ; do
+    echo "\033[1;32m$t..\033[0m"
+    libs/zk/test/$t
+done
